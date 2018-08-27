ae9d6c873ed6fec9b9a7b373c2c2e9193817348d
David McKissick <dwm0816@outlook.com>
2018-08-27 14:36:45 -0700

    Changed mapbox key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZXNwaXJpdHVzIiwiYSI6ImNqbDlxYzhnMTBpcWczcGw5NmxheW9ybW0ifQ.LhL5rY-uKmTzmJma1RRvQw',
+    mapboxToken: 'pk.eyJ1IjoiZXNwaXJpdHVzIiwiYSI6ImNqbGExbnBiejI4OG4zcXE4MGNtYTl4c3UifQ.CKLcp28OXb-pWoD_bKIaIA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
